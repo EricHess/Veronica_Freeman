f2c0e1715f80d3788e000fa386727605187c187e
Eric Hess <eric@fgi.com>
2014-08-20 21:31:15 -0700

    test update2

--- sites/all/themes/veronica/views-view--news--page.tpl.php
+++ sites/all/themes/veronica/views-view--news--page.tpl.php
@@ -19,7 +19,7 @@ foreach($nodeIds as $nodeId){
     if($node->status == 1){
 
         echo '<section class="newsItem" data-news-category="categoryHere">';
-            echo '<h3>'.$node->title.'</h3><>';
+            echo '<h3>'.$node->title.'</h3>';
             echo '<span class="newsDate">';
                 echo date("F jS, Y", $node->created);
             echo '</span>';
